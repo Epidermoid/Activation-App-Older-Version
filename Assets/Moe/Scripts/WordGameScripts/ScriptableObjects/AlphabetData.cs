465440bd963eca08b0dcf1030d1e5ad7f2c0a8e0
Mint <194066229+Moe-beep@users.noreply.github.com>
2026-03-09 14:06:06 +0200

    part 15 done

---
+++ Assets/Moe/Scripts/WordGameScripts/ScriptableObjects/AlphabetData.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+[CreateAssetMenu]
+public class AlphabetData : ScriptableObject
+{
+    [System.Serializable]
+    public class LetterData
+    {
+        public string letter;
+        public Sprite image;
+    }
+
+    public List<LetterData> AlphabetPlain = new List<LetterData>();
+    public List<LetterData> AlphabetNormal = new List<LetterData>();
+    public List<LetterData> AlphabetHilighted = new List<LetterData>();
+    public List<LetterData> AlphabetWrong = new List<LetterData>();
+}
