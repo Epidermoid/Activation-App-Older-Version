fdcea42f099327b8fa72b42e126d7014203830bc
Epidermoid <nikimikael.rapp@gmail.com>
2026-05-15 09:12:39 +0300

    achievement animation

--- Assets/Niki/Scripts/Achievement.cs
+++ Assets/Niki/Scripts/Achievement.cs
@@ -8,6 +8,8 @@ public class Achievement : MonoBehaviour
     [SerializeField] private string type;
 
     [SerializeField] private int id;
+
+    [SerializeField] private GameObject anim;
     
 
     private AchievementManager achievementManager;
@@ -26,6 +28,24 @@ public class Achievement : MonoBehaviour
         PlayerPrefs.SetInt("A" + id + "Completed", 1);
 
         PlayerPrefs.SetInt(type, PlayerPrefs.GetInt(type) + value);
+
+        var a = Instantiate(anim, GameObject.Find("Canvas").transform);
+        
+        var aP = a.GetComponent<AchievementAnimation>();
+        aP.value.text = value.ToString();
+        if (type == "Berry")
+        {
+            aP.image.sprite = aP.sprites[0];
+        }
+        else if (type == "RedBerry")
+        {
+            aP.image.sprite = aP.sprites[1];
+        }
+        else if (type == "GoldBerry")
+        {
+            aP.image.sprite = aP.sprites[2];
+        }
+        Destroy(a, 1.32f); 
 
         achievementManager.CheckUncheckedNotifs();
         achievementManager.CheckPendingAndComplete();
